3f276b845b71a38108ff6847795fb662280be83a
Shekhar Gulati <shekhargulati84@gmail.com>
2012-11-01 05:28:15 +0000

    added configuration for development as well

--- src/main/java/com/openshift/localjobs/config/DevMainConfig.java
+++ src/main/java/com/openshift/localjobs/config/DevMainConfig.java
@@ -2,7 +2,6 @@ package com.openshift.localjobs.config;
 
 import javax.sql.DataSource;
 
-import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
@@ -11,18 +10,16 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.authentication.UserCredentials;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseFactory;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.jdbc.datasource.init.DatabasePopulator;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.Database;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.social.connect.jdbc.JdbcUsersConnectionRepository;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -33,44 +30,16 @@ import com.mongodb.Mongo;
 @ComponentScan(basePackages = "com.openshift.localjobs", excludeFilters = { @Filter(Configuration.class) })
 @PropertySource("classpath:com/openshift/localjobs/config/application.properties")
 @EnableTransactionManagement
-@Profile("openshift")
-public class MainConfig {
+@Profile("dev")
+public class DevMainConfig {
 
-	@Bean
-	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-		LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
-		entityManagerFactory.setDataSource(dataSource());
-		entityManagerFactory.setPersistenceUnitName("postgresql");
-		entityManagerFactory.setPersistenceXmlLocation("classpath:com/openshift/localjobs/config/jpa-persistence.xml");
-		HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
-		hibernateJpaVendorAdapter.setDatabase(Database.POSTGRESQL);
-		hibernateJpaVendorAdapter.setGenerateDdl(true);
-		hibernateJpaVendorAdapter.setShowSql(true);
-		entityManagerFactory.setJpaVendorAdapter(hibernateJpaVendorAdapter);
-		return entityManagerFactory;
-	}
-
-	@Bean(destroyMethod = "close")
+	@Bean(destroyMethod = "shutdown")
 	public DataSource dataSource() {
-		String username = System.getenv("OPENSHIFT_DB_USERNAME");
-		String password = System.getenv("OPENSHIFT_DB_PASSWORD");
-		String host = System.getenv("OPENSHIFT_DB_HOST");
-		String port = System.getenv("OPENSHIFT_DB_PORT");
-		String url = "jdbc:postgresql://" + host + ":" + port + "/localjobs";
-		BasicDataSource dataSource = new BasicDataSource();
-		dataSource.setDriverClassName("org.postgresql.Driver");
-		dataSource.setUrl(url);
-		dataSource.setUsername(username);
-		dataSource.setPassword(password);
-		dataSource.setTestOnBorrow(true);
-		dataSource.setTestOnReturn(true);
-		dataSource.setTestWhileIdle(true);
-		dataSource.setTimeBetweenEvictionRunsMillis(1800000);
-		dataSource.setNumTestsPerEvictionRun(3);
-		dataSource.setMinEvictableIdleTimeMillis(1800000);
-		dataSource.setValidationQuery("SELECT version()");
-
-		return dataSource;
+		EmbeddedDatabaseFactory factory = new EmbeddedDatabaseFactory();
+		factory.setDatabaseName("localjobs");
+		factory.setDatabaseType(EmbeddedDatabaseType.H2);
+		factory.setDatabasePopulator(databasePopulator());
+		return factory.getDatabase();
 	}
 	
 	@Bean
@@ -101,8 +70,8 @@ public class MainConfig {
 
 	private DatabasePopulator databasePopulator() {
 		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-		populator.addScript(new ClassPathResource("drop-tables.sql",MainConfig.class));
-		populator.addScript(new ClassPathResource("create-tables.sql", MainConfig.class));
+		populator.addScript(new ClassPathResource("drop-tables.sql",DevMainConfig.class));
+		populator.addScript(new ClassPathResource("create-tables.sql", DevMainConfig.class));
 		populator.addScript(new ClassPathResource("JdbcUsersConnectionRepository.sql", JdbcUsersConnectionRepository.class));
 		return populator;
 	}
@@ -115,14 +84,10 @@ public class MainConfig {
 	
 	@Bean
 	public MongoDbFactory mongoDbFactory() throws Exception {
-		String openshiftMongoDbHost = System.getenv("OPENSHIFT_NOSQL_DB_HOST");
-		int openshiftMongoDbPort = Integer.parseInt(System.getenv("OPENSHIFT_NOSQL_DB_PORT"));
-		String username = System.getenv("OPENSHIFT_NOSQL_DB_USERNAME");
-		String password = System.getenv("OPENSHIFT_NOSQL_DB_PASSWORD");
-		Mongo mongo = new Mongo(openshiftMongoDbHost, openshiftMongoDbPort);
-		UserCredentials userCredentials = new UserCredentials(username,password);
-		String databaseName = "localjobs";
-		MongoDbFactory mongoDbFactory = new SimpleMongoDbFactory(mongo, databaseName, userCredentials);
+		Mongo mongo = new Mongo("localhost", 27017);
+		String databaseName = "jobsnearyou";
+		MongoDbFactory mongoDbFactory = new SimpleMongoDbFactory(mongo,
+				databaseName);
 		return mongoDbFactory;
 	}
 }
