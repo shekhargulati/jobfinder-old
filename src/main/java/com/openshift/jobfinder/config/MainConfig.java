8704c6aafddc7506aaf7436f4bbbe4cf56856e9d
Shekhar Gulati <shekhargulati84@gmail.com>
2012-11-14 17:37:06 +0530

    mongodb was local reffered

--- src/main/java/com/openshift/jobfinder/config/MainConfig.java
+++ src/main/java/com/openshift/jobfinder/config/MainConfig.java
@@ -114,23 +114,23 @@ public class MainConfig {
 		return mongoTemplate;
 	}
 	
-	@Bean
-	public MongoDbFactory mongoDbFactory() throws Exception {
-		Mongo mongo = new Mongo("localhost",27017);
-		MongoDbFactory mongoDbFactory = new SimpleMongoDbFactory(mongo, "jobfinder");
-		return mongoDbFactory;
-	}
-	
 //	@Bean
 //	public MongoDbFactory mongoDbFactory() throws Exception {
-//		String openshiftMongoDbHost = System.getenv("OPENSHIFT_MONGODB_DB_HOST");
-//		int openshiftMongoDbPort = Integer.parseInt(System.getenv("OPENSHIFT_MONGODB_DB_PORT"));
-//		String username = System.getenv("OPENSHIFT_MONGODB_DB_USERNAME");
-//		String password = System.getenv("OPENSHIFT_MONGODB_DB_PASSWORD");
-//		Mongo mongo = new Mongo(openshiftMongoDbHost, openshiftMongoDbPort);
-//		UserCredentials userCredentials = new UserCredentials(username,password);
-//		String databaseName = System.getenv("OPENSHIFT_APP_NAME");
-//		MongoDbFactory mongoDbFactory = new SimpleMongoDbFactory(mongo, databaseName, userCredentials);
+//		Mongo mongo = new Mongo("localhost",27017);
+//		MongoDbFactory mongoDbFactory = new SimpleMongoDbFactory(mongo, "jobfinder");
 //		return mongoDbFactory;
 //	}
+	
+	@Bean
+	public MongoDbFactory mongoDbFactory() throws Exception {
+		String openshiftMongoDbHost = System.getenv("OPENSHIFT_MONGODB_DB_HOST");
+		int openshiftMongoDbPort = Integer.parseInt(System.getenv("OPENSHIFT_MONGODB_DB_PORT"));
+		String username = System.getenv("OPENSHIFT_MONGODB_DB_USERNAME");
+		String password = System.getenv("OPENSHIFT_MONGODB_DB_PASSWORD");
+		Mongo mongo = new Mongo(openshiftMongoDbHost, openshiftMongoDbPort);
+		UserCredentials userCredentials = new UserCredentials(username,password);
+		String databaseName = System.getenv("OPENSHIFT_APP_NAME");
+		MongoDbFactory mongoDbFactory = new SimpleMongoDbFactory(mongo, databaseName, userCredentials);
+		return mongoDbFactory;
+	}
 }
