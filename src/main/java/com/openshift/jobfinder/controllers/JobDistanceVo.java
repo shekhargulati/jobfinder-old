374e0dde583d61380f8476292120732931cd49ab
Shekhar Gulati <shekhargulati84@gmail.com>
2012-11-27 14:11:09 +0530

    fixed for null pointer

--- src/main/java/com/openshift/jobfinder/controllers/JobDistanceVo.java
+++ src/main/java/com/openshift/jobfinder/controllers/JobDistanceVo.java
@@ -18,7 +18,7 @@ public class JobDistanceVo {
 	private String companyName;
 	private String[] skills;
 	private String address;
-	
+
 	private String distance;
 	private String duration;
 
@@ -32,17 +32,23 @@ public class JobDistanceVo {
 		this.skills = job.getSkills();
 		this.companyName = job.getCompany().getCompanyName();
 		this.address = job.getFormattedAddress();
-		this.distance = distance.getText();
-		this.duration = duration.getText();
+		if (distance != null) {
+			this.distance = distance.getText();
+		}
+		if(duration != null){
+			this.duration = duration.getText();
+		}
+		
 	}
 
 	public void setJobId(String jobId) {
 		this.jobId = jobId;
 	}
+
 	public String getJobId() {
 		return jobId;
 	}
-	
+
 	public String getCompanyName() {
 		return companyName;
 	}
@@ -92,18 +98,18 @@ public class JobDistanceVo {
 	}
 
 	public static JobDistanceVo fromJson(String json) {
-		return new JSONDeserializer<JobDistanceVo>()
-				.use(null, JobDistanceVo.class).deserialize(json);
+		return new JSONDeserializer<JobDistanceVo>().use(null,
+				JobDistanceVo.class).deserialize(json);
 	}
 
 	public static String toJsonArray(Collection<JobDistanceVo> collection) {
-		return new JSONSerializer().include("job").exclude("*.class").serialize(collection);
+		return new JSONSerializer().include("job").exclude("*.class")
+				.serialize(collection);
 	}
 
 	public static Collection<JobDistanceVo> fromJsonArray(String json) {
-		return new JSONDeserializer<List<JobDistanceVo>>()
-				.use(null, ArrayList.class)
-				.deserialize(json);
+		return new JSONDeserializer<List<JobDistanceVo>>().use(null,
+				ArrayList.class).deserialize(json);
 	}
 
 }
